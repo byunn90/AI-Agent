ae303adc82954953beec53c5142caf124e388e2a
Kayhan Mamak <kayhan.mamak@hotmail.com>
2025-10-13 13:46:30 +1100

    Testing again

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from dotenv import load_dotenv
 from google import genai
 # Load environment variables from .env
 load_dotenv()
-
+print("Hello testing")
 api_key = os.environ.get("GEMINI_API_KEY")
 
 
 client = genai.Client(api_key=api_key)
 
 response = client.models.generate_content(
-    model='gemini-2.5-flash', contents='"Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."'
+    model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
 )
 print(response.text)
 
