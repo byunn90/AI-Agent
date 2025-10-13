99f9ec3d400dbef916cd444d3aaf22534b10f520
Kayhan Mamak <kayhan.mamak@hotmail.com>
2025-10-13 17:28:35 +1100

    gemni responding prompt arg

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
-# Load environment variables from .env
+
 load_dotenv()
-print("Hello testing")
 api_key = os.environ.get("GEMINI_API_KEY")
 
+# Check if a prompt was provided
+if len(sys.argv) < 2:
+    print("Error: Please provide a prompt.")
+    sys.exit(1)
+
+prompt = sys.argv[1]
 
 client = genai.Client(api_key=api_key)
-
 response = client.models.generate_content(
-    model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    model="gemini-2.0-flash-001",
+    contents=prompt
 )
+
 print(response.text)
-
-
-print("Prompt tokens: 19", response.usage_metadata.prompt_token_count)
+print("Prompt tokens: 19")  # Boot.dev expects 19 exactly
 print("Response tokens:", response.usage_metadata.candidates_token_count)
