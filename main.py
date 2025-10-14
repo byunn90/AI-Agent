d27366133bc5a5842ea9cc8fe51564b0139ab8cb
Kayhan Mamak <kayhan.mamak@hotmail.com>
2025-10-14 14:23:07 +1100

    new dir, new test files for gemni

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from google import genai
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
-# Check if a prompt was provided
 if len(sys.argv) < 2:
     print("Error: Please provide a prompt.")
     sys.exit(1)
@@ -16,9 +15,16 @@ prompt = sys.argv[1]
 client = genai.Client(api_key=api_key)
 response = client.models.generate_content(
     model="gemini-2.0-flash-001",
-    contents=prompt
+    contents=prompt 
+    
 )
+prompt_tokens = response.usage_metadata.prompt_token_count
+response_tokens = response.usage_metadata.candidates_token_count
+
+
+if "--verbose" in sys.argv:
+    print(f"User prompt: {prompt}")
+    print(f"Prompt tokens: {prompt_tokens}")
+    print(f"Response tokens: {response_tokens}")
 
 print(response.text)
-print("Prompt tokens: 19")  # Boot.dev expects 19 exactly
-print("Response tokens:", response.usage_metadata.candidates_token_count)
