48830a0c61e6b29c2f607c57df22144e6e36e5dc
Kayhan Mamak <kayhan.mamak@hotmail.com>
2025-10-15 15:41:59 +1100

    f.read contents

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,9 +2,7 @@ import os
 
 def get_files_info(working_directory, directory="."):
     full_path = os.path.abspath(os.path.join(working_directory, directory))
-    path_size = os.path.getsize(full_path)
 
-    is_dir = os.path.isdir(full_path)
     lines = []
 
     if not full_path.startswith(os.path.abspath(working_directory)):
@@ -13,14 +11,35 @@ def get_files_info(working_directory, directory="."):
     if not os.path.isdir(full_path):
         return f'Error: "{directory}" is not a directory'
 
-    for i in os.listdir(full_path):
-        print("test", i)
-        lines.append(i)
+    for item in os.listdir(full_path):
+        item_path = os.path.join(full_path, item)
+        size = os.path.getsize(item_path)
+        is_dir = os.path.isdir(item_path)
+        lines.append(f"- {item}: file_size={size} bytes, is_dir={is_dir}")
 
-
-    get_files = os.listdir(full_path)
-
-    x = print(f"file_size: {path_size} bytes, is_dir={is_dir}")
-    return x
+    
+    return "\n".join(lines)
 
 print(get_files_info(".", "functions"))
+
+
+
+def get_file_content(working_directory, file_path):
+    full_path = os.path.abspath(os.path.join(working_directory, file_path))
+    list = []
+
+    if not full_path.startswith(os.path.abspath(working_directory)):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(full_path):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    MAX_CHARS = 10000
+
+    with open(file_path, "r") as f:
+        file_content_string = f.read(MAX_CHARS)
+
+
+    
+
+    
+
+print(get_file_content(".", "functions"))
