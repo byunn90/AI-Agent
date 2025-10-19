79ab3667f80b9958f531f397d1a41503159eb796
Kayhan Mamak <kayhan.mamak@hotmail.com>
2025-10-19 20:06:01 +1100

    tests failing for std stderr

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -1,10 +1,11 @@
 import os
+import subprocess
 
 def run_python_file(working_directory, file_path, args=[]):
     timer = 30
-    full_path = abspath.os.path(join.os.path(working_directory, file))
+    full_path = os.path.abspath(os.path.join(working_directory, file_path))
 
-    if not full_path.startswith(abspath.os.path(working_directory)):
+    if not full_path.startswith(os.path.abspath(working_directory)):
         return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
     if not os.path.exists(full_path):
         return f'Error: File "{file_path}" not found.'
@@ -12,18 +13,29 @@ def run_python_file(working_directory, file_path, args=[]):
         return f'Error: "{file_path}" is not a Python file.'
     try:
         completed = subprocess.run(
-        ["python3", full_path, *args],   
+        ["python3", full_path, *args],
         capture_output=True,             
         text=True,                      
         timeout=30,                      
         cwd=working_directory            
         )
-        print(completed.stdout)
-        print(completed.stderr)
-        print(returncode)
+        print("stdout:", repr(completed.stdout))
+        print("stderr:", repr(completed.stderr))
+        print("returncode:", completed.returncode)
+        out = completed.STDOUT
+        er = completed.stderr
+        code = completed.returncode
+        if out == "" and er == ""
+            return "No output produced"
+
+        if completed.returncode != 0:
+            return "Process exited with code f{code}"
+        else:
+            return f"STDOUT: {out}\nSTDERR: {er}"
+
     except Exception as e:
-        return f"Error: {e}"
-print(run_python_file)
+        return f"Error: executing Python file: {e}"
+        
  
 
 
