ff9af83b679787319ce9af50ecf18dd3e3c1adfb
Kayhan Mamak <kayhan.mamak@hotmail.com>
2025-10-20 15:06:02 +1100

    clean up

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -4,7 +4,6 @@ import subprocess
 def run_python_file(working_directory, file_path, args=[]):
     timer = 30
     full_path = os.path.abspath(os.path.join(working_directory, file_path))
-
     if not full_path.startswith(os.path.abspath(working_directory)):
         return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
     if not os.path.exists(full_path):
@@ -16,16 +15,13 @@ def run_python_file(working_directory, file_path, args=[]):
         ["python3", full_path, *args],
         capture_output=True,             
         text=True,                      
-        timeout=30,                      
+        timeout=timer,                      
         cwd=working_directory            
         )
-        print("stdout:", repr(completed.stdout))
-        print("stderr:", repr(completed.stderr))
-        print("returncode:", completed.returncode)
-        out = completed.STDOUT
-        er = completed.stderr
+        out = completed.stdout
+        er  = completed.stderr
         code = completed.returncode
-        if out == "" and er == ""
+        if out == "" and er == "":
             return "No output produced"
 
         if completed.returncode != 0:
